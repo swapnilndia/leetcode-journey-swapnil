30f40c9764eacee5db969a43372f6da93cea1851
Swapnil Katiyar <swapnil240695@gmail.com>
2026-02-20 06:23:08 +0530

    reverse integer solved at both places

---
+++ src/namaste-dsa/Warm Up/reverseInteger.js
@@ -0,0 +1,15 @@
+// Write a function to reverse a number;
+
+function reverseNumber(number) {
+    let reverse = 0;
+    let absoluteNumber = Math.abs(number);
+
+    while (absoluteNumber > 0) {
+        let lastDigit = absoluteNumber % 10;
+        reverse = (reverse * 10) + lastDigit;
+        absoluteNumber = Math.floor(absoluteNumber / 10)
+    }
+    return number < 0 ? -reverse : reverse;
+}
+
+console.log(reverseNumber(-1234)); 
