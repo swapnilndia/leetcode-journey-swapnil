cc878ed617c6b71255dfed99795477cc81eab276
swapnilndia <48873666+swapnilndia@users.noreply.github.com>
2026-02-22 14:48:18 +0530

    Chore/setup eslint prettier husky (#10)
    * chore: configure eslint, prettier, husky and commitlint

--- src/namaste-dsa/Warm Up/countDigit.js
+++ src/namaste-dsa/Warm Up/countDigit.js
@@ -1,22 +1,23 @@
 // write a functin that return the count of digits in a number
 
-const numsArray = [-121, 31, 14562, 346346, 1125787696959, 0]
+const numsArray = [-121, 31, 14562, 346346, 1125787696959, 0];
 
 function countDigitsOfNumInArray(arr) {
-    if (arr.length < 1) return "no elements found in the arr"
-    for (let i = 0; i < arr.length; i++) {
-        let count = 0;
-        let num = Math.abs( arr[i]) 
-        if (num === 0) {
-            console.log(`${num} has 1 digit`)
-            continue;
-        }
-        while (num > 0) {
-            num = Math.floor(num / 10)
-            count++;
-        }
-        console.log(`${arr[i]} has ${count} digits`);
+  if (arr.length < 1) return 'no elements found in the arr';
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+
+    let num = Math.abs(arr[i]);
+    if (num === 0) {
+      console.log(`${num} has 1 digit`);
+      continue;
     }
-    return;
+    while (num > 0) {
+      num = Math.floor(num / 10);
+      count++;
+    }
+    console.log(`${arr[i]} has ${count} digits`);
+  }
+  return;
 }
-countDigitsOfNumInArray(numsArray)
+countDigitsOfNumInArray(numsArray);
