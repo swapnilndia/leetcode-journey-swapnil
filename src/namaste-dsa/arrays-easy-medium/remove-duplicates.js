533f8d0c21ffb1cb444db79d00d96e307c226e4f
Swapnil Katiyar <swapnil240695@gmail.com>
2026-02-23 06:07:31 +0530

    feat: solved unique elements in array by in-place exchange

---
+++ src/namaste-dsa/arrays-easy-medium/remove-duplicates.js
@@ -0,0 +1,18 @@
+// Write a function to remove duplicated elements from an array.
+const nums = [1,1,2];
+
+// This is 2 pointers approach.
+
+function removeDuplicate(nums) {
+    let scanner = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[scanner]) {
+            scanner = scanner + 1;
+            nums[scanner] = nums[i]
+
+        }
+    }
+    console.log(scanner);
+    return nums
+}
+console.log(removeDuplicate(nums));
