4cd9aa0d284b5aefcfee5fe7ef0da2a600dd62a2
swapnilndia <48873666+swapnilndia@users.noreply.github.com>
2026-02-26 07:08:14 +0530

    Move zeros 283 (#18)
    * feat: move zeros problem solved on leetcode
    
    * feat: move zeros problem solved on leetcode

---
+++ src/leetcode/easy/move-zeros-283.js
@@ -0,0 +1,33 @@
+/**
+ * Problem: Move Zeroes
+ * Difficulty: Easy
+ *
+ * Approach:
+ * - Traverse the array from the start.
+ * - Also create a new variable, which will keep the track of the zero.
+ * - As we encounter any non-zero number while traversing, we will swap it with the zero.
+ * - Return the array at last
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * Key Learning:
+ * - Properly scan the array for zero.
+ */
+
+const numArr = [0, 1];
+
+function moveZeros(nums) {
+  let numShifted = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[numShifted] = nums[i];
+      numShifted++;
+    }
+  }
+  for (let i = numShifted; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+}
+
+moveZeros(numArr);
