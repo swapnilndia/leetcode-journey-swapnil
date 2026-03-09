86073d64180cce70a3e9978fc6e56d2233646bdb
Swapnil Katiyar <swapnil240695@gmail.com>
2026-03-09 07:23:12 +0530

    feat: added question for recursion

--- src/namaste-dsa/recursion/recursion-101.js
+++ src/namaste-dsa/recursion/recursion-101.js
@@ -8,3 +8,24 @@
  * 1. Base case: the stopping condition (when to stop recursing).
  * 2. Recursive case: where the function calls itself.
  */
+
+// Write a recursive function that prints value of n to 1;
+
+function recursivePrint(n) {
+  if (n < 1) return;
+  console.log(n);
+  n = n - 1;
+  recursivePrint(n);
+}
+
+recursivePrint(10);
+
+// Write a recursive functin that prints 1 to n;
+let n = 15;
+function recursivePrint2(x) {
+  if (x > n) return;
+  console.log(x);
+  recursivePrint2(x + 1);
+}
+
+recursivePrint2(1);
